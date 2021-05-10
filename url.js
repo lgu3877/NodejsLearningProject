67c11129d84515a668675990513c7b332abf2144
lgu3877 <lgu3877@naver.com>
2021-05-10 16:20:57 +0900

    chore : url.js
    url 주석 추가
    
    20210510 16:20

--- url.js
+++ url.js
@@ -15,10 +15,10 @@ console.log('url.format() : ', url.format(parsedUrl));
 /*
 
 -url.parse
-주소를 분해함ㄴ query가 있습니다.
+주소를 분해함 query가 있습니다.
 WHATWG 방식과 비교하면 username과 password 대신 auth 속성이 있고, searchParams 대시
 
--url.format
+-url.format술
 WHATWG 방식 url과 기존 노드의 url을 모두 사용할 수 있습니다.
 분해되었던 url 객체를 다시 원래 상태로 조립합니다.
 
